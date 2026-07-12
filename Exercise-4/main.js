e77991d2dbea7c20451877bbcf0642fd6f51e1f4
Musharraf-Mahad <musharafmahad17@gmail.com>
2026-07-12 23:15:07 +0300

    submit exercise-4

---
+++ Exercise-4/main.js
@@ -0,0 +1,20 @@
+
+// Function declartion
+function add(x,y) {
+    return x + y;
+}
+
+console.log(add(5,6));
+console.log(add(30,6));
+console.log(add(25,3));
+console.log(add(15,4));
+
+
+//Function Expression
+let substract = function(a,b) {
+    return a - b;
+}
+
+console.log(substract(8,9));
+console.log(substract(33,4));
+console.log(substract(9,21));

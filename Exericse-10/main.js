e3c93d595dec68bc89de5186f5bc7c8e0f0dc160
Musharraf-Mahad <musharafmahad17@gmail.com>
2026-07-13 12:08:57 +0300

    submit exercise-10

---
+++ Exericse-10/main.js
@@ -0,0 +1,11 @@
+let library = [
+    {
+        title: "chemsitry" , author: "Msuharraf M." , year: 2023
+    },
+    {
+        title: "Physicis" , author: "Hassan A." , year: 2023
+    }
+];
+
+    console.log(library[0].title);
+    console.log(library[1].author);

1c175ee80b1793ee712667d153c6979f0601e26e
Musharraf-Mahad <musharafmahad17@gmail.com>
2026-07-13 11:17:14 +0300

    submit-exercis-7

---
+++ Exercise-7/main.js
@@ -0,0 +1,11 @@
+let colors = ['Red' , 'Blue' , 'Green'];
+
+console.log("Before: " + colors);
+
+//push
+colors.push("yellow");
+console.log("After-push: " + colors);
+
+//pop
+colors.pop(colors);
+console.log("After-pop: " + colors);

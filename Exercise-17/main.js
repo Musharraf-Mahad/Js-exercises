770748edcd0a921d0c6698148a2819ee83a5ddc0
Musharraf-Mahad <musharafmahad17@gmail.com>
2026-07-14 17:08:54 +0300

    submit exercise-17

---
+++ Exercise-17/main.js
@@ -0,0 +1,10 @@
+let temp = 16;
+if(temp <= 0) {
+    console.log("it is very cold");
+} else if(temp > 0 && temp < 15) {
+    console.log("it is cold");
+} else if(temp >= 15 && temp <=25) {
+    console.log("it is warm");
+} else {
+    console.log("it is hot");
+}

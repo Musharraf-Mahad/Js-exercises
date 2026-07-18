ffde2f0e8af33dbf1838e325a8bd2b94c09c44d0
Musharraf-Mahad <musharafmahad17@gmail.com>
2026-07-18 17:17:20 +0300

    submit exercise-32

---
+++ Exercise-32/main.js
@@ -0,0 +1,9 @@
+
+    let header = document.querySelector('#header'); //by id
+    console.log(header);
+
+    let name = document.querySelector('h3'); //by tag
+    console.log(name);
+
+    let text = document.querySelectorAll('.text');  //by class
+    console.log(text);
